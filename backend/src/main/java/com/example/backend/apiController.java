b8abf94aed4b4bb322bf5dad208c12a56f9dae67
PaytonDennis <102490243+PaytonDennis@users.noreply.github.com>
2025-07-18 21:13:33 -0600

    updated api controller

--- backend/src/main/java/com/example/backend/apiController.java
+++ backend/src/main/java/com/example/backend/apiController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class OopPracticeController {
+public class ApiController {
     @GetMapping("/api/oop-message")
     public String getOopMessage() {
         return "Hello, OOP Practice!";
